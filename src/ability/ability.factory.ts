3c242fb42c7b589b142cba36bbd89f3c7a040ab4
Luqman Rasat <luqmanrasat@hotmail.com>
2022-08-08 18:41:33 +0800

    create ability module

---
+++ src/ability/ability.factory.ts
@@ -0,0 +1,41 @@
+import {
+  Ability,
+  AbilityBuilder,
+  AbilityClass,
+  ExtractSubjectType,
+  InferSubjects,
+} from '@casl/ability';
+import { Injectable } from '@nestjs/common';
+import { User } from '../users/entities/user.entity';
+
+export enum Action {
+  MANAGE = 'manage',
+  CREATE = 'create',
+  READ = 'read',
+  UPDATE = 'update',
+  DELETE = 'delete',
+}
+
+export type Subjects = InferSubjects<typeof User> | 'all';
+
+export type AppAbility = Ability<[Action, Subjects]>;
+
+@Injectable()
+export class AbilityFactory {
+  defineAbility(user: User) {
+    const { can, cannot, build } = new AbilityBuilder(
+      Ability as AbilityClass<AppAbility>,
+    );
+
+    if (user.isAdmin) {
+      can(Action.MANAGE, 'all');
+    } else {
+      can(Action.READ, 'all');
+    }
+
+    return build({
+      detectSubjectType: (subject) =>
+        subject.constructor as ExtractSubjectType<Subjects>,
+    });
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AbilityModule } from './ability/ability.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, AbilityModule],
   controllers: [AppController],
   providers: [AppService],
 })
